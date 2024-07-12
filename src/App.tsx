c1af0d8152e0899e13b87dd5d6c4a023c64649ce
unknown <mateus.r2000@gmail.com>
2024-07-12 13:04:33 -0300

    first-commit/Journey

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6 text-center">
+        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+        <div>
+
+        </div>
+        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+        com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade.</a>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
